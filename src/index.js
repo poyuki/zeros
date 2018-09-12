197142f981256833ae57202ce051df86fa201e2c
poyuKiryl <poyuki9@gmail.com>
2018-09-12 12:29:08 +0300

    Zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    let res=0;
+    let currentVal=number-(number%5);
+    while(currentVal>1){
+        currentVal=(currentVal-(currentVal%5))/5;
+        res+=currentVal;
+    }
+
+    return res;
+};
